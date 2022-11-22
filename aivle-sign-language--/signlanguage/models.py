018eeb9c75be5c106405ecd7bb56d9bcd16748a1
yejig <110088882+yejig@users.noreply.github.com>
2022-11-22 16:33:24 +0900

    Update models.py

--- aivle-sign-language--/signlanguage/models.py
+++ aivle-sign-language--/signlanguage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# from picklefield.fields import PickledObjectField
 
 # Create your models here.
 
@@ -7,3 +8,23 @@ class Result(models.Model):
     answer = models.CharField(max_length=10)
     result = models.CharField(max_length=10)
     pub_date = models.DateTimeField('date published')
+
+    
+class AI_Model(models.Model):
+    # realModel = PickledObjectField()
+    model_Name = models.CharField(max_length=50)
+    model_Version = models.CharField(max_length=50, null=True)
+    model_File = models.FileField(upload_to='modelFiles/')
+    create_Date = models.DateField(auto_now=True)
+    is_selected = models.BooleanField(default=True)
+    
+    # if is_selected:
+        
+    # def save(self, *args, **kwargs):
+    #     if self.is_selected:
+    #         self.objects.filter(is_selected=True).update(is_selected=False)
+    #     super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'Name : {self.model_Name}, Date : {self.create_Date}'
+
